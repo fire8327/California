17a990bcbfb487a65e6b77fe59abb131fb561d4a
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-01-02 09:34:22 +0000

    create ideas

--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -2,7 +2,8 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        'figmatext':'4B4B4B',
+        'figmatext':'#4B4B4B',
+        'figmabg1':'#F8F9FA',
         },
       },
   },
